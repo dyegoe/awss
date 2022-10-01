0f02126bc6c74b80b07bd3744b6c432dca7d153d
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-10-01 19:28:02 +0300

    refactor cmd ec2 to a single function

--- cmd/ec2.go
+++ cmd/ec2.go
@@ -16,11 +16,11 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/dyegoe/awss/search"
 	"github.com/spf13/cobra"
 )
 
+var ec2Ids string
 var ec2Names string
 var ec2PrivateIps string
 var ec2PublicIps string
@@ -30,18 +30,30 @@ var ec2Cmd = &cobra.Command{
 	Use:   "ec2",
 	Short: "Use it to search across EC2 instances.",
 	Long: `Use it to search across EC2 instances.
-	       You can search by name, by private IPs or public IPs.`,
+	       You can search by ids, names, private IPs or public IPs.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		s := search.Instances{
+			Profile: profile,
+			Region:  region,
+		}
+		if ec2Ids != "" {
+			s.Search("ids", ec2Ids)
+			s.JSON()
+			return
+		}
 		if ec2Names != "" {
-			ec2NamesSearch()
+			s.Search("names", ec2Names)
+			s.JSON()
 			return
 		}
 		if ec2PrivateIps != "" {
-			ec2PrivateIpsSearch()
+			s.Search("private-ips", ec2PrivateIps)
+			s.JSON()
 			return
 		}
 		if ec2PublicIps != "" {
-			ec2PublicIpsSearch()
+			s.Search("public-ips", ec2PublicIps)
+			s.JSON()
 			return
 		}
 	},
@@ -50,28 +62,8 @@ var ec2Cmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(ec2Cmd)
 
+	ec2Cmd.Flags().StringVar(&ec2Ids, "ids", "", "Provide a list of comma-separated ids. e.g. --ids `i-00000000000000001,i-00000000000000002`")
 	ec2Cmd.Flags().StringVar(&ec2Names, "names", "", "Provide a list of comma-separated names. It searchs using the 'tag:Name'. e.g. --names `instance-1,instance-2`")
-	ec2Cmd.Flags().StringVar(&ec2PrivateIps, "privateIps", "", "Provide a list of comma-separated private IPs. e.g. --privateIps `172.16.0.1,172.17.1.254`")
-	ec2Cmd.Flags().StringVar(&ec2PublicIps, "publicIps", "", "Provide a list of comma-separated public IPs. e.g. --publicIps `52.28.19.20,52.30.31.32`")
-}
-
-func ec2NamesSearch() {
-	fmt.Println("---- ec2 ----")
-	fmt.Println("Profile:", profile)
-	fmt.Println("Region:", region)
-	fmt.Println("Names:", ec2Names)
-}
-
-func ec2PrivateIpsSearch() {
-	fmt.Println("---- ec2 ----")
-	fmt.Println("Profile:", profile)
-	fmt.Println("Region:", region)
-	fmt.Println("Private IPs:", ec2PrivateIps)
-}
-
-func ec2PublicIpsSearch() {
-	fmt.Println("---- ec2 ----")
-	fmt.Println("Profile:", profile)
-	fmt.Println("Region:", region)
-	fmt.Println("Public IPs:", ec2PublicIps)
+	ec2Cmd.Flags().StringVar(&ec2PrivateIps, "private-ips", "", "Provide a list of comma-separated private IPs. e.g. --private-ips `172.16.0.1,172.17.1.254`")
+	ec2Cmd.Flags().StringVar(&ec2PublicIps, "public-ips", "", "Provide a list of comma-separated public IPs. e.g. --public-ips `52.28.19.20,52.30.31.32`")
 }
