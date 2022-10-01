34b65a3ec5fed650f941e3c0c751679a528ca351
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-10-01 19:32:27 +0300

    add file headers

--- search/search.go
+++ search/search.go
@@ -1,3 +1,18 @@
+/*
+Copyright © 2022 Dyego Alexandre Eugenio dyegoe@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package search
 
 import (
@@ -8,10 +23,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
+// getConfig returns a new AWS config.
 func getConfig(profile, region string) (aws.Config, error) {
 	return config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile), config.WithRegion(region))
 }
 
+// split returns a slice of strings.
 func split(input string) []string {
 	return strings.Split(input, ",")
 }
