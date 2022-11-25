ff7586f96259b33010a2fcdd60f5345ba4052d54
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2022-11-25 20:15:47 +0200

    Release v0.5.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [v0.5.6] - 2022-11-25
+
 <!-- markdownlint-disable MD024 -->
 ### Changed
 
--- commands/commands.go
+++ commands/commands.go
@@ -34,7 +34,7 @@ You can also pass 'all' to iterate over all regions.
 You can find the source code on GitHub:
 https://github.com/dyegoe/awss`,
 	// Remember to update this version when releasing a new version
-	Version: "0.5.5",
+	Version: "0.5.6",
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		err := initConfig()
 		if err != nil {
